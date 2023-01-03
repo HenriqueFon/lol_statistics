e617b1589718f0f7585565daa5ca050984b45aa1
henriqueFon <henriquearaujo091@gmail.com>
2023-01-03 16:10:13 -0300

    Reorganizing code

--- src/components/ChampionCaroseul/ChampionCarousel.js
+++ src/components/ChampionCaroseul/ChampionCarousel.js
@@ -1,13 +1,28 @@
 import { useState, useEffect } from "react";
 import Data from "../../Data/Data";
+import { getData } from "../../API/getData";
 import { Champion_index } from "../../Data/Champion_index";
 import { motion } from "framer-motion";
 import './ChampionCaroseul.css';
 
-const ChampionCarousel = ({data}) => {
+
+const ChampionCarousel = () => {
+
+    const [data, setData] = useState([]);
+    const [names, setNames] = useState([]);
     const [championSplash, setCHampionSplash] = useState([]);
 
-    let names = data.map(elements =>  Champion_index(elements));
+    const getFreeWeekAsync = async () => {
+        console.log('rodei')
+        let dados = await getData('free_rotation', '');
+        setData(dados.freeChampionIds);
+        console.log(dados);
+    }
+
+    const createArrayOfNames = async (data) => {
+        let namesArray = await data.map(elements =>  Champion_index(elements));
+        setNames(namesArray);
+    }
 
     const getChampionSplash = (names) => {
         let image = names.map(element =>{return `${Data('champion_splash', '')}${element}_0.jpg`});
@@ -15,18 +30,22 @@ const ChampionCarousel = ({data}) => {
         setCHampionSplash(image)
     }
 
-    const doRequisitions = async (names) => {
+    const doRequisitions = async () => {
+        await getFreeWeekAsync();
+        
+        // let names = ['Jinx', 'Talon', 'Zed']
+        console.log(names)
         await getChampionSplash(names);
     }
 
     useEffect(() => {
-        doRequisitions(names);
+        doRequisitions();
     },[]);
 
     return (
        <motion.div className = 'carousel' whileTap = {{cursor:"grabbing"}}>
-            <motion.div className = 'inner' 
-                drag = "x" 
+            <motion.div className = 'inner'
+                drag = "x"
                 initial = {{x:100}}
                 animate = {{x: 0}}
                 transition = {{duration: 0.8}}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,12 @@
-import { useState, useEffect } from "react";
-import ChampionCaroseulData from "../../components/ChampionCaroseul/ChampionCaroseulData";
+import ChampionCaroseul from "../../components/ChampionCaroseul/ChampionCarousel";
 import NameForm from "../../components/NameForm/NameForm";
-import  Data  from "../../Data/Data";
-import { Champion_index } from "../../Data/Champion_index";
 
 const Home = () => {
 
     return (
         <div>
             <h1>Mascoot Stats</h1>
-            <ChampionCaroseulData />
+            <ChampionCaroseul />
             <NameForm />
         </div>
     )
